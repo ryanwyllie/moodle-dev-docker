deeb45aa9c2a535244d31d27664512bdfe878800
Ryan Wyllie <ryan@ryanwyllie.com>
2018-11-28 13:56:27 +0800

    fixed some stuff

--- moodle-config.php
+++ moodle-config.php
@@ -1,9 +1,7 @@
 <?php  // Moodle configuration file
-
 unset($CFG);
 global $CFG;
 $CFG = new stdClass();
-
 $CFG->dbtype    = getenv('MOODLE_DOCKER_DBTYPE');
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = getenv('MOODLE_DOCKER_DBHOST');
@@ -12,10 +10,9 @@ $CFG->dbuser    = getenv('MOODLE_DOCKER_DBUSER');
 $CFG->dbpass    = getenv('MOODLE_DOCKER_DBPASS');
 $CFG->prefix    = getenv('MOODLE_DOCKER_DBPREFIX');
 $CFG->dboptions = ['dbcollation' => getenv('MOODLE_DOCKER_DBCOLLATION')];
-
 $host = 'localhost';
-if (!empty(getenv('VIRTUAL_HOST'))) {
-    $host = getenv('VIRTUAL_HOST');
+if (!empty(getenv('MOODLE_DOCKER_WEB_HOST'))) {
+    $host = getenv('MOODLE_DOCKER_WEB_HOST');
 }
 $CFG->wwwroot   = "http://{$host}";
 $port = getenv('MOODLE_DOCKER_WEB_PORT');
@@ -26,7 +23,6 @@ $CFG->dataroot  = '/var/www/moodledata';
 $CFG->admin     = 'admin';
 $CFG->directorypermissions = 0777;
 $CFG->smtphosts = 'mailhog:1025';
-
 // Debug options - possible to be controlled by flag in future..
 $CFG->debug = (E_ALL | E_STRICT); // DEBUG_DEVELOPER
 $CFG->debugdisplay = 1;
@@ -35,14 +31,12 @@ $CFG->perfdebug = 15;
 $CFG->debugpageinfo = 1;
 $CFG->allowthemechangeonurl = 1;
 $CFG->passwordpolicy = 0;
-
 $CFG->phpunit_dataroot  = '/var/www/phpunitdata';
-$CFG->phpunit_prefix = 't_' . getenv('MOODLE_DOCKER_DBPASS');
+$CFG->phpunit_prefix = 't_' . getenv('MOODLE_DOCKER_DBPREFIX');
 define('TEST_EXTERNAL_FILES_HTTP_URL', 'http://exttests');
-
-$CFG->behat_wwwroot   = "http://webserver";
+$CFG->behat_wwwroot   = 'http://webserver';
 $CFG->behat_dataroot  = '/var/www/behatdata';
-$CFG->behat_prefix = 'b_' . getenv('MOODLE_DOCKER_DBPASS');
+$CFG->behat_prefix = 'b_' . getenv('MOODLE_DOCKER_DBPREFIX');
 $CFG->behat_profiles = array(
     'default' => array(
         'browser' => getenv('MOODLE_DOCKER_BROWSER'),
@@ -50,36 +44,27 @@ $CFG->behat_profiles = array(
     ),
 );
 $CFG->behat_faildump_path = '/var/www/behatfaildumps';
-
 define('PHPUNIT_LONGTEST', true);
-
 if (getenv('MOODLE_DOCKER_PHPUNIT_EXTRAS')) {
     define('TEST_SEARCH_SOLR_HOSTNAME', 'solr');
     define('TEST_SEARCH_SOLR_INDEXNAME', 'test');
     define('TEST_SEARCH_SOLR_PORT', 8983);
-
     define('TEST_SESSION_REDIS_HOST', 'redis');
     define('TEST_CACHESTORE_REDIS_TESTSERVERS', 'redis');
-
     define('TEST_CACHESTORE_MONGODB_TESTSERVER', 'mongodb://mongo:27017');
-
     define('TEST_CACHESTORE_MEMCACHED_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
     define('TEST_CACHESTORE_MEMCACHE_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
-
     define('TEST_LDAPLIB_HOST_URL', 'ldap://ldap');
     define('TEST_LDAPLIB_BIND_DN', 'cn=admin,dc=openstack,dc=org');
     define('TEST_LDAPLIB_BIND_PW', 'password');
     define('TEST_LDAPLIB_DOMAIN', 'ou=Users,dc=openstack,dc=org');
-
     define('TEST_AUTH_LDAP_HOST_URL', 'ldap://ldap');
     define('TEST_AUTH_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
     define('TEST_AUTH_LDAP_BIND_PW', 'password');
     define('TEST_AUTH_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
-
     define('TEST_ENROL_LDAP_HOST_URL', 'ldap://ldap');
     define('TEST_ENROL_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
     define('TEST_ENROL_LDAP_BIND_PW', 'password');
     define('TEST_ENROL_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
 }
-
 require_once(__DIR__ . '/lib/setup.php');
